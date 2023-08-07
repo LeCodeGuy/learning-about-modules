e8876ed9cf53f8e5e52c7b03ecf3c0b77b113c48
LeCodeGuy <devanj89@gmail.com>
2023-08-07 11:55:53 +0200

    added figlet to my solution

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,14 @@
 import chalk from 'chalk';
+import figlet from "figlet";
 //import the greet module that is in the current folder
 import greet from './greet.js'
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+// const styledMessage = chalk.bgGreen.black(greet('Xola'));
+// console.log(styledMessage)
+
+figlet.text(
+    greet('Xola'),
+  function (err, data) {
+    console.log(chalk.bgGreen.black(data));
+  }
+);
